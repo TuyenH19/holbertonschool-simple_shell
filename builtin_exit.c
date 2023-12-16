6f32421a8770e9833e36476f99503c6d97e248a2
TuyenH19 <thtuyen199@gmail.com>
2023-12-16 23:31:50 +0000

    create builtin functions

---
+++ builtin_exit.c
@@ -0,0 +1,13 @@
+#include"shell.h"
+
+/**
+ * builtin_exit - function to exit to terminal
+ * @args: arguments
+ * Return: 0 to terminate the process
+*/
+int builtin_exit(char **args)
+{
+	(void)args;
+
+	return (0);
+}
